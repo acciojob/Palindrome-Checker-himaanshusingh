6693974195268bc8641da3b3e4468c51d0ff3771
Himanshu Singh <himanshusingh1k@gmail.com>
2026-02-10 09:26:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// complete the given function
-
-function palindrome(str){
-
+function palindrome(str) {
+  return str === str.split("").reverse().join("");
 }
-module.exports = palindrome
+
+module.exports = palindrome;
